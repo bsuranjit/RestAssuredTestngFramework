f8bc4c5d5fd065eefd61cb64ff98a327721dd045
suranjit biswas <you@example.com>
2024-12-24 01:09:43 -0500

    updated dummy class

--- src/test/java/com/spotify/oauth2/utils/Dummyclass.java
+++ src/test/java/com/spotify/oauth2/utils/Dummyclass.java
@@ -2,7 +2,7 @@ package com.spotify.oauth2.utils;
 
 public class Dummyclass {
 	public void dummy() {
-		System.out.println("this is new dummy class");
+		System.out.println("this is  dummy file");
 	}
 
 }
