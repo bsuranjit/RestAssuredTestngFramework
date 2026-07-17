97fcf3eceaf3eb878cacc5e99e34c2b55cf247a0
suranjit biswas <you@example.com>
2026-07-17 01:36:23 -0400

    fix: update 401 error message to match expired token response

--- src/test/java/com/spotify/oauth2/api/StatusCode.java
+++ src/test/java/com/spotify/oauth2/api/StatusCode.java
@@ -4,8 +4,7 @@ public enum StatusCode {
 	CODE_200(200,""),
 	CODE_201(201,""),
 	CODE_400(400,"Missing required field: name"),
-	//CODE_401(401,"Invalid access token");
-	CODE_401(401,"Missing/invalid/expired access token");
+	CODE_401(401,"Invalid access token");
 	
 	//private final int code;
 	//private final String msg;
