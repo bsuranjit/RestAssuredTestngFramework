7cf12d2ea7356d124b4f7b32bab6a884048971d7
suranjit biswas <you@example.com>
2026-07-17 00:41:20 -0400

    fix: update 401 error payload message in StatusCode enum

--- src/test/java/com/spotify/oauth2/api/StatusCode.java
+++ src/test/java/com/spotify/oauth2/api/StatusCode.java
@@ -4,7 +4,8 @@ public enum StatusCode {
 	CODE_200(200,""),
 	CODE_201(201,""),
 	CODE_400(400,"Missing required field: name"),
-	CODE_401(401,"Invalid access token");
+	//CODE_401(401,"Invalid access token");
+	CODE_401(401,"Missing/invalid/expired access token");
 	
 	//private final int code;
 	//private final String msg;
